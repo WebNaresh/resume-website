cee151373a03491395a75aaa014e40faa7c39b8d
Naresh Bhosale <nareshbhosale73@gmail.com>
2023-08-08 23:29:45 +0530

    completed

--- app/(combine)/resume/page.tsx
+++ app/(combine)/resume/page.tsx
@@ -1,10 +1,24 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
 type Props = {};
 
 const Resume = (props: Props) => {
   return (
-    <div>
-      <h1 className="hello mx-16 my-8 relative text-2xl ">My Resume</h1>
-    </div>
+    <section className="mx-16">
+      <h1 className="hello  my-8 relative text-2xl ">My Resume</h1>
+      <div className="flex">
+        <Image
+          src={"/resume-screen.png"}
+          height={200}
+          width={400}
+          alt="resume"
+        />
+        <a href="/resumenew.pdf" download="resumenew.pdf">
+          <Button className="w-full m-4">Download Resume</Button>
+        </a>
+      </div>
+    </section>
   );
 };
 
